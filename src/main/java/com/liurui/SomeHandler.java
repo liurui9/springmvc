efa1c112e0628c2ae685b7cf03906066d568b42a
liurui9 <87374925@qq.com>
2022-11-09 18:10:43 +0800

    first commit

---
+++ src/main/java/com/liurui/SomeHandler.java
@@ -0,0 +1,14 @@
+package com.liurui;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class SomeHandler implements Controller {
+    @Override
+    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
+        ModelAndView mv = new ModelAndView();
+       mv.addObject("message","hello,springmvc");
+        mv.setViewName("welcome");
+        return mv;
+    }
+}
